ced0b0381ca9121fcc92c36eaca51ddcd76d99a7
laxmanpache <pachelaxman22@gmail.com>
2021-11-20 22:47:08 +0530

    tryed to add responsivness

--- laxman-portfolio/src/PageHeader/PageHeader.js
+++ laxman-portfolio/src/PageHeader/PageHeader.js
@@ -2,7 +2,7 @@ import React from 'react'
 import '../PageHeader/PageHeader.css';
 const PageHeader = (props) => {
     return (
-        <div className="container">
+        <div className="container col-md-auto">
             <hr/>
             <h1 className="title">{props.title}</h1>
         </div>
